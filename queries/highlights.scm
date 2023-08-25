083e51084bb5e44e406c52f11e32078d4ba42cf9
plusgut <github@webentwickler2-0.de>
2023-08-25 11:49:10 +0200

    feat(highlighting): added delimiter highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -3,13 +3,20 @@
 (algebraicDataTypeDeclaration 
   [ "data" ] @keyword.storage.type
   name: (identifier) @type.enum
+  "=" @operator
   value: (algebraicDataTypeValue
     name: (identifier) @constructor
+    "(" @punctuation.bracket
+    ","* @punctuation.delimiter
+    ")" @punctuation.bracket
   )
   (
     "," @punctuation.delimiter
     value: (algebraicDataTypeValue
      name: (identifier) @constructor
+    "(" @punctuation.bracket
+    ","* @punctuation.delimiter
+    ")" @punctuation.bracket
     )
   )*
 )
